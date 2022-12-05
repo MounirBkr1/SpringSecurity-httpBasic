6ea873208d2b4f8a207fa08b62c4e698002dde1f
Mounir Bkr <miirbri1@gmail.com>
2022-12-05 22:15:35 +0100

    commit 0 "test"

---
+++ src/test/java/com/mnr/springsecurityhttpbasic/SpringSecurityHttpBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.springsecurityhttpbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityHttpBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
